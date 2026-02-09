8b54dad3a1789e7d7598870e8d9f07627bc3436e
kjungw <sagakkb@gmail.com>
2026-02-09 16:53:49 +0900

    test: 테스트 환경에 Redis 추가

--- backend/src/test/java/com/pickeat/backend/support/AcceptanceTest.java
+++ backend/src/test/java/com/pickeat/backend/support/AcceptanceTest.java
@@ -9,12 +9,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
 
     private static final MySQLContainer<?> MYSQL_CONTAINER;
+    private static final GenericContainer<?> REDIS_CONTAINER;
 
     @Autowired
     private DatabaseCleaner databaseCleaner;
@@ -40,6 +43,11 @@ public class AcceptanceTest {
                 .withPassword("test")
                 .withCommand("--log-bin-trust-function-creators=1");
         MYSQL_CONTAINER.start();
+
+        REDIS_CONTAINER = new GenericContainer<>(DockerImageName.parse("redis:latest"))
+                .withExposedPorts(6379)
+                .withCommand("redis-server --requirepass pickeat123");
+        REDIS_CONTAINER.start();
     }
 
     @DynamicPropertySource
@@ -48,5 +56,9 @@ public class AcceptanceTest {
         registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
         registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
         registry.add("spring.jpa.hibernate.ddl-auto", () -> "none");
+
+        registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
+        registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379));
+        registry.add("spring.data.redis.password", () -> "pickeat123");
     }
 }
--- backend/src/test/java/com/pickeat/backend/support/DatabaseSliceTest.java
+++ backend/src/test/java/com/pickeat/backend/support/DatabaseSliceTest.java
@@ -1,19 +1,24 @@
 package com.pickeat.backend.support;
 
+import com.pickeat.backend.global.config.RedisConfig;
 import com.pickeat.backend.support.utility.DatabaseCleaner;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @DataJpaTest
-@Import({DatabaseCleaner.class})
+@Import({DatabaseCleaner.class, RedisConfig.class, RedisAutoConfiguration.class})
 public class DatabaseSliceTest {
 
     private static final MySQLContainer<?> MYSQL_CONTAINER;
+    private static final GenericContainer<?> REDIS_CONTAINER;
 
     @Autowired
     private DatabaseCleaner databaseCleaner;
@@ -30,6 +35,11 @@ public class DatabaseSliceTest {
                 .withPassword("test")
                 .withCommand("--log-bin-trust-function-creators=1");
         MYSQL_CONTAINER.start();
+
+        REDIS_CONTAINER = new GenericContainer<>(DockerImageName.parse("redis:latest"))
+                .withExposedPorts(6379)
+                .withCommand("redis-server --requirepass pickeat123");
+        REDIS_CONTAINER.start();
     }
 
     @DynamicPropertySource
@@ -38,5 +48,9 @@ public class DatabaseSliceTest {
         registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
         registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
         registry.add("spring.jpa.hibernate.ddl-auto", () -> "none");
+
+        registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
+        registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379));
+        registry.add("spring.data.redis.password", () -> "pickeat123");
     }
 }
